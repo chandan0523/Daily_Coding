5f32388eb367ec38430bf9fcbe8bb437f33a6d78
Chandan <chandan297003@gmail.com>
2026-01-20 22:56:15 +0530

    DAY5

---
+++ Day5/script.js
@@ -0,0 +1,8 @@
+let year = prompt("Enter a year:");
+year = Number(year);
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+}
